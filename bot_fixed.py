9abd3e91edc0f7ab300d8a00abf488cf700cfa18
alijonov-ae <yangiciskk@gmail.com>
2026-08-29 10:15:13 +0500

    Fix formatting of InlineKeyboardButton list
    Signed-off-by: alijonov-ae <yangiciskk@gmail.com>

--- bot_fixed.py
+++ bot_fixed.py
@@ -570,7 +570,7 @@ def useful_bots_keyboard():
         [InlineKeyboardButton("📩 Admin bilan bog'lanish", callback_data="contact_admin")],
         [InlineKeyboardButton("🪄 Fon o'chirish", callback_data="bg_remove"),
          InlineKeyboardButton("📐 4K rasm", callback_data="upscale_4k")],
-        [InlineKeyboardButton("🔥 Avto-reaksiya", callback_data="admin_avto_reaction"),
+        [InlineKeyboardButton("🔥 Avto-reaksiya", callback_data="admin_avto_reaction")],
         [InlineKeyboardButton("🔙 Orqaga", callback_data="back_main")],
     ]
     return InlineKeyboardMarkup(keyboard)
